0342df9a41a249b477a425b3a887d10f376a76a4
Sharku Jang <pjsw007@naver.com>
2023-06-11 20:11:52 +0900

    데이터베이스 연결/해제 기능 추가

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 # app.py
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Response
+import sqlite3
 import os
+import gzip
+import zlib
 
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'records'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DB_CONNECTION'] = None
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
@@ -14,19 +18,46 @@ if not os.path.exists(UPLOAD_FOLDER):
 def index():
     return render_template('index.html')
 
+@app.route('/connect',  methods=['POST'])
+def connect_db():
+    if len(request.get_json()) <= 0:
+        return Response("Invalid request", status=400)
+    db_name = request.get_json()['id']
+    if len(db_name) <= 0:
+        return Response("Invalid database name", status=400)
+
+    conn = app.config['DB_CONNECTION']
+    if conn is not None:
+        print("Close previous connection to database")
+        conn.close()
+
+    try:
+        sqlite3.connect(f"{db_name}.db")
+        return Response(f"Connected to db {db_name}", status=201)
+    except Exception as e:
+        return Response(f"{e}", status=400)
+    
+@app.route('/disconnect', methods=['GET','POST'])
+def disconnect_db():
+    conn = app.config['DB_CONNECTION']
+    if conn is None:
+        return Response("No valid connection to database. Ignore request...", status=200)
+    conn.close()
+    return Response("Disconnect to database successfully", status=200)
+
 @app.route('/audio', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
-        return 'No file part'
+        return Response('Invalid File data', status=400)
     
     file = request.files['file']
     
     if file.filename == '':
-        return 'No selected file'
+        return Response('Invalid file name', status=400)
     
     if file:
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-        return 'File successfully uploaded'
+        return Response('File successfully uploaded', status=200)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000)
